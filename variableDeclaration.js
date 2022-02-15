f1b93e1d6a2c77a375822ec2766b826d8b37046c
Majey <kevinerick78@yahoo.com>
2022-02-15 19:06:55 +0300

    var, let and const

---
+++ variableDeclaration.js
@@ -0,0 +1,24 @@
+//********************************** VAR, LET & CONST ************************** */
+
+/*
+
+var name = "Kevin";
+var name = "Erick";
+console.log(name);
+
+*/
+//**************************************************** */
+
+/*
+
+let name = "Kevin";
+let name = "Erick";
+console.log(name); 
+// Logs name has already been declared. A variable can be reinitialized but can't be redeclared
+// With const, a variable can only be declared and be initialized once
+
+*/
+
+// more examples on differences : https://youtu.be/YB0h_2fuPs8?list=PLQcBFrxTul9L2GqfunTPcndQkNo_gxxJS
+
+
