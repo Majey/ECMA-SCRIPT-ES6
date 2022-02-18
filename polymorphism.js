be9361d9b1ea5d702a633a2bfd4fcd2384ed1501
Majey <kevinerick78@yahoo.com>
2022-02-18 20:23:32 +0300

    Polymorphism concept

---
+++ polymorphism.js
@@ -0,0 +1,28 @@
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    getName(){
+        return `My name is ${this.name}`;
+    }
+}
+
+class Student extends Person {  
+    getName = () => {
+        console.log(super.getName());
+        return `The student's name is ${this.name}`
+    };  
+    getAge = () => `I am ${this.age} years old`;    
+}
+
+class Teacher extends Person {
+    getExperience = () => `I have ${this.age} years experience`;
+}
+
+const stude = new Student("Kevin", 30);
+console.log(stude.getName());
+console.log(stude.getAge());
+const teach = new Teacher("Mr. Chris", 10);
+console.log(teach.getName());
+console.log(teach.getExperience());
