2eb1cd9418528624b70d293e3cd2e37c94ba0473
Majey <kevinerick78@yahoo.com>
2022-02-19 12:13:25 +0300

    Modules concept

--- module1.js
+++ module1.js
@@ -1,5 +1,5 @@
-import { message, getName, myClass } from "./module2.js";
+import { message as msg, getName, myClass } from "./module2.js";
 
-console.log(message);
+console.log(msg);
 console.log(getName("Kevin"));
 let obj = new myClass();
