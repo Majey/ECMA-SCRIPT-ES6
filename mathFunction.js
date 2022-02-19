2048419efeb9857b389e5a310d68c2faafceda01
Majey <kevinerick78@yahoo.com>
2022-02-19 10:36:10 +0300

    Math.sign() & Math.trunc()

---
+++ mathFunction.js
@@ -0,0 +1,6 @@
+let value = -5.7;
+console.log(Math.sign(value));
+// -1 means the number is a negative and 1 means its a positive. 0 means its a neutral number
+console.log(Math.trunc(value));
+console.log(Math.floor(value));
+console.log(Math.ceil(value));
